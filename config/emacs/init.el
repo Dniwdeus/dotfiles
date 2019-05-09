e51bf585264c78aa38c0a218ba7e81a146845003
dniwdeus <dniwdeus@argo>
2019-05-09 14:52:09 +0200

    feat: enable org-plus for emacs

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -22,6 +22,16 @@
 ;; load global configs for emacs
 (load-file "~/.emacs.d/elisp/global-settings.el")
 
+;; install org plus contrib with either package.el[fn:12]
+
+(unless package-archive-contents    ;; Refresh the packages descriptions
+  (package-refresh-contents))
+(setq package-load-list '(all))     ;; List of packages to load
+(unless (package-installed-p 'org-plus-contrib)  ;; Make sure the Org package is
+  (package-install 'org-plus-contrib))           ;; installed, install it if not
+(package-initialize)                ;; Initialize & Install Package
+
+
 ;; load restclient
 (use-package restclient
   :ensure t
@@ -247,7 +257,7 @@ h1,h2,h3{line-height:1.2}")
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (which-key magit use-package))))
+ '(package-selected-packages (quote (org-plus-contrib which-key magit use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
