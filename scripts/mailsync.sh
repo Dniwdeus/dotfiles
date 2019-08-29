c195436f3c8d4e3dd310e40420fd0c5ee7c232f3
dniwdeus <dniwdeus@argo>
2019-08-29 11:21:25 +0200

    feat(mail): setup mailsync for syncing mails via IMAP

---
+++ scripts/mailsync.sh
@@ -0,0 +1,30 @@
+MBSYNC=$(pgrep mbsync)
+NOTMUCH=$(pgrep notmuch)
+STATE=`nmcli networking connectivity`
+
+if [ $STATE = 'full' ]
+then
+  if [ -n "$MBSYNC" -o -n "$NOTMUCH" ]; then
+    echo "Already running one instance of mail-sync. Exiting..."
+    exit 0
+  fi
+
+  MDIR="$(ls $MAILDIR/)"
+  /usr/bin/mbsync -Va
+  /usr/bin/notmuch new
+
+  for mdir in $MDIR; do
+    echo "Processing $mdir"
+    echo "Tagging $mdir"
+    notmuch tag +${mdir} -- folder:$mdir/
+    for fdir in $(ls -d ${MAILDIR}$mdir/*); do
+      if [ $(basename $fdir) != "INBOX" ] && [ $(basename $fdir) != "new" ]; then
+        echo "Tagging for $(basename $fdir)"
+        notmuch tag +$(basename $fdir) -inbox -- folder:$mdir/$(basename $fdir)
+      fi
+    done
+  done
+  exit 0
+fi
+echo "No Internets!"
+exit 0
