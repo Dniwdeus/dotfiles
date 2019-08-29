b40fb1d3c95151095cabcfe37512ed1a3ad03935
dniwdeus <dniwdeus@argo>
2019-08-29 11:14:07 +0200

    polish(emacs): add yaml-mode and helms-mode (notmuch) to used packages

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -444,7 +444,9 @@ X-Message-SMTP-Method: sendmail
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (org-plus-contrib which-key magit use-package))))
+ '(package-selected-packages
+   (quote
+    (yaml-mode helm-notmuch notmuch org-plus-contrib which-key magit use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
