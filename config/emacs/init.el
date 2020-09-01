cd2b816dba14a2ba625d79743f8a88ac25709804
dniwdeus <dniwdeus@argo>
2020-09-01 15:49:50 +0200

    emacs: set melpa source-URL to HTTPS
    so far nothing broke

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -2,7 +2,7 @@
 (require 'package)
 (setq package-enable-at-startup t)
 
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
